bfb97568b626c1664069c6d3591c7f738fb44673
frognar <sebastian.przyszlak@gmail.com>
2024-07-07 11:10:05 +0200

    Enable advanced Query Syntax in asynchronous context (from _ in _  from _ in _  select _)

--- src/dotResult/ResultExtensions.cs
+++ src/dotResult/ResultExtensions.cs
@@ -41,4 +41,87 @@ public static class ResultExtensions
         var result = await source.ConfigureAwait(false);
         return await result.MapAsync(selector).ConfigureAwait(false);
     }
+
+    /// <summary>
+    /// Asynchronously projects the value of a successful result into a new result, flattens the result, and applies a final projection.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in the original Result.</typeparam>
+    /// <typeparam name="TResult">The type of the value in the resulting Result.</typeparam>
+    /// <typeparam name="TIntermediate">The intermediate type used in the projection.</typeparam>
+    /// <param name="source">The task representing the original result.</param>
+    /// <param name="selector">The function to project the value into another Result.</param>
+    /// <param name="projector">The function to transform the original value and the intermediate value into the final value.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a new Result with the final projected value if the original and intermediate results were successful; otherwise, a failure.</returns>
+    /// <remarks>This method enables query syntax for the Result type.</remarks>
+    public static async Task<Result<TResult>> SelectMany<T, TResult, TIntermediate>(
+        this Task<Result<T>> source,
+        Func<T, Result<TIntermediate>> selector,
+        Func<T, TIntermediate, TResult> projector)
+    {
+        var result = await source.ConfigureAwait(false);
+        return result.FlatMap(x => selector(x).Map(y => projector(x, y)));
+    }
+
+    /// <summary>
+    /// Asynchronously projects the value of a successful result into a new result using an asynchronous projector, flattens the result, and applies a final projection.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in the original Result.</typeparam>
+    /// <typeparam name="TResult">The type of the value in the resulting Result.</typeparam>
+    /// <typeparam name="TIntermediate">The intermediate type used in the projection.</typeparam>
+    /// <param name="source">The task representing the original result.</param>
+    /// <param name="selector">The function to project the value into another Result.</param>
+    /// <param name="projector">The asynchronous function to transform the original value and the intermediate value into the final value.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a new Result with the final projected value if the original and intermediate results were successful; otherwise, a failure.</returns>
+    /// <remarks>This method enables query syntax for the Result type.</remarks>
+    public static async Task<Result<TResult>> SelectMany<T, TResult, TIntermediate>(
+        this Task<Result<T>> source,
+        Func<T, Result<TIntermediate>> selector,
+        Func<T, TIntermediate, Task<TResult>> projector)
+    {
+        var result = await source.ConfigureAwait(false);
+        return await result.FlatMapAsync(x => selector(x).MapAsync(y => projector(x, y))).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Asynchronously projects the value of a successful result using an asynchronous selector, flattens the result, and applies a final projection.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in the original Result.</typeparam>
+    /// <typeparam name="TResult">The type of the value in the resulting Result.</typeparam>
+    /// <typeparam name="TIntermediate">The intermediate type used in the projection.</typeparam>
+    /// <param name="source">The task representing the original result.</param>
+    /// <param name="selector">The asynchronous function to project the value into another Result.</param>
+    /// <param name="projector">The function to transform the original value and the intermediate value into the final value.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a new Result with the final projected value if the original and intermediate results were successful; otherwise, a failure.</returns>
+    /// <remarks>This method enables query syntax for the Result type.</remarks>
+    public static async Task<Result<TResult>> SelectMany<T, TResult, TIntermediate>(
+        this Task<Result<T>> source,
+        Func<T, Task<Result<TIntermediate>>> selector,
+        Func<T, TIntermediate, TResult> projector)
+    {
+        var result = await source.ConfigureAwait(false);
+        return await result.FlatMapAsync(async x => (await selector(x).ConfigureAwait(false)).Map(y => projector(x, y)));
+    }
+
+    /// <summary>
+    /// Asynchronously projects the value of a successful result using an asynchronous selector, flattens the result, and applies a final asynchronous projection.
+    /// </summary>
+    /// <typeparam name="T">The type of the value in the original Result.</typeparam>
+    /// <typeparam name="TResult">The type of the value in the resulting Result.</typeparam>
+    /// <typeparam name="TIntermediate">The intermediate type used in the projection.</typeparam>
+    /// <param name="source">The task representing the original result.</param>
+    /// <param name="selector">The asynchronous function to project the value into another Result.</param>
+    /// <param name="projector">The asynchronous function to transform the original value and the intermediate value into the final value.</param>
+    /// <returns>A task that represents the asynchronous operation, containing a new Result with the final projected value if the original and intermediate results were successful; otherwise, a failure.</returns>
+    /// <remarks>This method enables query syntax for the Result type.</remarks>
+    public static async Task<Result<TResult>> SelectMany<T, TResult, TIntermediate>(
+        this Task<Result<T>> source,
+        Func<T, Task<Result<TIntermediate>>> selector,
+        Func<T, TIntermediate, Task<TResult>> projector)
+    {
+        var result = await source.ConfigureAwait(false);
+        return await result.FlatMapAsync(
+                async x => await (await selector(x).ConfigureAwait(false))
+                    .MapAsync(y => projector(x, y)))
+            .ConfigureAwait(false);
+    }
 }
--- tests/dotResult.Tests.Unit/ResultQuerySyntaxTests.cs
+++ tests/dotResult.Tests.Unit/ResultQuerySyntaxTests.cs
@@ -62,6 +62,96 @@ public class ResultQuerySyntaxTests
             .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
     }
 
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithFinalFunctionReturningTask(int value, int otherValue)
+    {
+        var result = await (
+            from v1 in Validate(value)
+            from v2 in Validate(otherValue)
+            select Task.FromResult(v1 + v2));
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithIntermediateFunctionReturningTask(int value, int otherValue)
+    {
+        var result = await (
+            from v1 in Validate(value)
+            from v2 in Task.FromResult(Validate(otherValue))
+            select v1 + v2);
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithBothFunctionsReturningTask(int value, int otherValue)
+    {
+        var result = await (
+            from v1 in Validate(value)
+            from v2 in Task.FromResult(Validate(otherValue))
+            select Task.FromResult(v1 + v2));
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueInTaskResult(int value, int otherValue)
+    {
+        var result = await (
+            from v1 in Task.FromResult(Validate(value))
+            from v2 in Validate(otherValue)
+            select v1 + v2);
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithFinalFunctionReturningTaskInTaskResult(
+        int value,
+        int otherValue)
+    {
+        var result = await (
+            from v1 in Task.FromResult(Validate(value))
+            from v2 in Validate(otherValue)
+            select Task.FromResult(v1 + v2));
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithIntermediateFunctionReturningTaskInTaskResult(
+        int value,
+        int otherValue)
+    {
+        var result = await (
+            from v1 in Task.FromResult(Validate(value))
+            from v2 in Task.FromResult(Validate(otherValue))
+            select v1 + v2);
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    [Property]
+    public async Task CanUseQuerySyntaxToFlatMapValueWithBothFunctionsReturningTaskInTaskResult(
+        int value,
+        int otherValue)
+    {
+        var result = await (
+            from v1 in Task.FromResult(Validate(value))
+            from v2 in Task.FromResult(Validate(otherValue))
+            select Task.FromResult(v1 + v2));
+
+        result.Should()
+            .Be(Validate(value).FlatMap(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
     private static Result<int> Validate(int value) => value > 0
         ? Success.From(value)
         : Fail.OfType<int>(Failure.Fatal(message: "Value is not positive."));
