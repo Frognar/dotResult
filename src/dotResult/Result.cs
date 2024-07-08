666822c9de00b4050b464ba27fe0ca030f8f0422
frognar <sebastian.przyszlak@gmail.com>
2024-07-08 10:04:57 +0200

    Add Map2 function

--- src/dotResult/Result.cs
+++ src/dotResult/Result.cs
@@ -247,3 +247,30 @@ public readonly record struct Result<T>
         public Failure Value { get; } = Value;
     }
 }
+
+/// <summary>
+/// Provides static methods for working with Result types.
+/// </summary>
+public static class Result
+{
+    /// <summary>
+    /// Combines two results and applies a mapping function if both results are successful.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value in the first result.</typeparam>
+    /// <typeparam name="TOtherValue">The type of the value in the second result.</typeparam>
+    /// <typeparam name="TResult">The type of the value in the resulting result.</typeparam>
+    /// <param name="result1">The first result.</param>
+    /// <param name="result2">The second result.</param>
+    /// <param name="map">The function to apply to the values of both results if they are successful.</param>
+    /// <returns>A new result containing the mapped value if both results are successful; otherwise, a failure result.</returns>
+    public static Result<TResult> Map2<TValue, TOtherValue, TResult>(
+        Result<TValue> result1,
+        Result<TOtherValue> result2,
+        Func<TValue, TOtherValue, TResult> map)
+    {
+        return
+            from v1 in result1
+            from v2 in result2
+            select map(v1, v2);
+    }
+}
--- tests/dotResult.Tests.Unit/ResultMappingTests.cs
+++ tests/dotResult.Tests.Unit/ResultMappingTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using FsCheck;
 using FsCheck.Xunit;
+using Result = DotResult.Result;
 
 namespace dotResult.Tests.Unit;
 
@@ -44,4 +45,16 @@ public class ResultMappingTests
             .Should()
             .Be(Fail.OfType<int>(failure));
     }
+
+    [Property]
+    public void CanTransformTwoValuesFromResultUsingMap2(int value, int otherValue)
+    {
+        Result.Map2(Validate(value), Validate(otherValue), (v1, v2) => v1 + v2)
+            .Should()
+            .Be(Validate(value).Bind(v1 => Validate(otherValue).Map(v2 => v1 + v2)));
+    }
+
+    private static Result<int> Validate(int value) => value > 0
+        ? Success.From(value)
+        : Fail.OfType<int>(Failure.Fatal(message: "Value is not positive."));
 }
