568d08a4892b925245f950685a08969620b2ede3
frognar <sebastian.przyszlak@gmail.com>
2024-07-08 09:54:36 +0200

    Add IsFailure property to Result

--- tests/dotResult.Tests.Unit/ResultPropTests.cs
+++ tests/dotResult.Tests.Unit/ResultPropTests.cs
@@ -15,6 +15,15 @@ public class ResultPropTests
             .Be(value > 0);
     }
 
+    [Property]
+    public void IsFailureShouldBeTrueWhenResultIsInFailureState(int value)
+    {
+        Validate(value)
+            .IsFailure
+            .Should()
+            .Be(value <= 0);
+    }
+
     private static Result<int> Validate(int value) => value > 0
         ? Success.From(value)
         : Fail.OfType<int>(Failure.Fatal(message: "Value is not positive."));
