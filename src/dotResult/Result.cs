a4fc40b90fd90a0074dc6fd1aab7374c2d72d9fc
frognar <sebastian.przyszlak@gmail.com>
2024-07-14 16:18:02 +0200

    Move flatten Extension method to separate file

--- src/dotResult/Result.cs
+++ src/dotResult/Result.cs
@@ -334,17 +334,6 @@ public static class Result
     }
 
     /// <summary>
-    /// Flattens a nested <see cref="Result{T}"/> by binding the inner result to the outer result.
-    /// </summary>
-    /// <typeparam name="T">The type of the value in the result.</typeparam>
-    /// <param name="nested">The nested result to flatten.</param>
-    /// <returns>The flattened result containing the inner value if both the outer and inner results are successful; otherwise, a failure result.</returns>
-    public static Result<T> Flatten<T>(this Result<Result<T>> nested)
-    {
-        return nested.Bind(v => v);
-    }
-
-    /// <summary>
     /// Converts a value to a successful <see cref="Result{T}"/>.
     /// </summary>
     /// <typeparam name="T">The type of the value.</typeparam>
