d7309a9b123d14c14532fb63f3e062550cd365b0
frognar <sebastian.przyszlak@gmail.com>
2024-07-24 09:07:35 +0200

    Add Fold method for Result

---
+++ tests/dotResult.Tests.Unit/ResultFoldingTests.cs
@@ -0,0 +1,26 @@
+﻿using DotResult;
+using FluentAssertions;
+using FsCheck.Xunit;
+
+namespace dotResult.Tests.Unit;
+
+public class ResultFoldingTests
+{
+    [Property]
+    public void CanUpdateStateWithSuccessResult(int state, int value)
+    {
+        Success.From(value)
+            .Fold(state, (s, v) => s + v)
+            .Should()
+            .Be(state + value);
+    }
+
+    [Property]
+    public void CanReturnUnchangedStateWithFailureResult(int state)
+    {
+        Fail.OfType<int>(Failure.Validation())
+            .Fold(state, (s, v) => s + v)
+            .Should()
+            .Be(state);
+    }
+}
