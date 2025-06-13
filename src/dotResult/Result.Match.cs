e2036755b2746fb99ecbbece7eca6047e58607b0
frognar <sebastian.przyszlak@gmail.com>
2025-06-13 11:10:06 +0200

    Update Match methods to handle multiple failures
    Refactored `Match` and `MatchAsync` methods to process `IEnumerable<Failure>` instead of a single `Failure`, enabling support for multiple failures. Updated corresponding unit tests to validate the handling of multiple failures.

--- src/dotResult/Result.Match.cs
+++ src/dotResult/Result.Match.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,12 +14,12 @@ public readonly partial record struct Result<T>
     /// <param name="failure">Function to execute if the result is a failure.</param>
     /// <param name="success">Function to execute if the result is a success.</param>
     /// <returns>The result of the executed function.</returns>
-    public TResult Match<TResult>(Func<Failure, TResult> failure, Func<T, TResult> success)
+    public TResult Match<TResult>(Func<IEnumerable<Failure>, TResult> failure, Func<T, TResult> success)
     {
         return _result switch
         {
             SuccessType successType => success(successType.Value),
-            FailureType failureType => failure(failureType.Value.First()),
+            FailureType failureType => failure(failureType.Value),
             _ => throw new InvalidOperationException("Reached an invalid state in Match."),
         };
     }
@@ -30,12 +31,12 @@ public readonly partial record struct Result<T>
     /// <param name="failure">Asynchronous function to execute if the result is a failure.</param>
     /// <param name="success">Asynchronous function to execute if the result is a success.</param>
     /// <returns>A task that represents the asynchronous operation, containing the result of the executed function.</returns>
-    public async Task<TResult> MatchAsync<TResult>(Func<Failure, Task<TResult>> failure, Func<T, Task<TResult>> success)
+    public async Task<TResult> MatchAsync<TResult>(Func<IEnumerable<Failure>, Task<TResult>> failure, Func<T, Task<TResult>> success)
     {
         return _result switch
         {
             SuccessType successType => await success(successType.Value).ConfigureAwait(false),
-            FailureType failureType => await failure(failureType.Value.First()).ConfigureAwait(false),
+            FailureType failureType => await failure(failureType.Value).ConfigureAwait(false),
             _ => throw new InvalidOperationException("Reached an invalid state in Match."),
         };
     }
@@ -47,12 +48,12 @@ public readonly partial record struct Result<T>
     /// <param name="failure">Function to execute if the result is a failure.</param>
     /// <param name="success">Asynchronous function to execute if the result is a success.</param>
     /// <returns>A task that represents the asynchronous operation, containing the result of the executed function.</returns>
-    public async Task<TResult> MatchAsync<TResult>(Func<Failure, TResult> failure, Func<T, Task<TResult>> success)
+    public async Task<TResult> MatchAsync<TResult>(Func<IEnumerable<Failure>, TResult> failure, Func<T, Task<TResult>> success)
     {
         return _result switch
         {
             SuccessType successType => await success(successType.Value).ConfigureAwait(false),
-            FailureType failureType => failure(failureType.Value.First()),
+            FailureType failureType => failure(failureType.Value),
             _ => throw new InvalidOperationException("Reached an invalid state in Match."),
         };
     }
