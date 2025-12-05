8b31d065a812617d7d53f15d65975a8bfe993b06
frognar <sebastian.przyszlak@gmail.com>
2025-12-05 10:38:24 +0100

    Add test case for `Map` not invoking mapper on error in `ResultMapTests.cs`

--- tests/dotResult.Tests.Unit/ResultMapTests.cs
+++ tests/dotResult.Tests.Unit/ResultMapTests.cs
@@ -20,7 +20,16 @@ public class ResultMapTests
     [Property]
     public Property Map_respects_composition() =>
         Prop.ForAll<int, Func<int, int>, Func<int, int>>((value, f, g) =>
-            Ok(value).Map(f).Map(g) == Ok(value).Map(x => g(f(x))));    
+            Ok(value).Map(f).Map(g) == Ok(value).Map(x => g(f(x))));
+
+    [Property]
+    public Property Map_does_not_invoke_mapper_on_error() =>
+        Prop.ForAll<NonEmptyString>(msg =>
+        {
+            var called = false;
+            var result = Error(msg.Get).Map(x => { called = true; return x + 1; });
+            return result == Error(msg.Get) && called == false;
+        });
 
     private static Result<int, string> Ok(int value) => DotResult.Result.Ok<int, string>(value);
     private static Result<int, string> Error(string value) => DotResult.Result.Error<int, string>(value);
