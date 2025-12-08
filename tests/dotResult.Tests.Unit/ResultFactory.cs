08a643d2b349b85077470567b1d0e423c55e6e58
frognar <sebastian.przyszlak@gmail.com>
2025-12-08 08:05:47 +0100

    Add `Ok` and `Error` overloads for generic types in `ResultFactory.cs`

--- tests/dotResult.Tests.Unit/ResultFactory.cs
+++ tests/dotResult.Tests.Unit/ResultFactory.cs
@@ -4,4 +4,6 @@ internal static class ResultFactory
 {
     internal static Result<int, string> Ok(int value) => Result.Ok<int, string>(value);
     internal static Result<int, string> Error(string value) => Result.Error<int, string>(value);
+    internal static Result<T, TError> Ok<T, TError>(T value) => Result.Ok<T, TError>(value);
+    internal static Result<T, TError> Error<T, TError>(TError value) => Result.Error<T, TError>(value);
 }
